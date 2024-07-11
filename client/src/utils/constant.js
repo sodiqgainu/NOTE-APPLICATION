2377c7e02ca7a47b90987978e642084efece4065
sodiqganiu <sodiqganiu412@gmail.com>
2024-07-11 17:04:27 +0100

    whole commit

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,2 +1,2 @@
 // export const  BASE_URL = "http://localhost:3000";
-export const  BASE_URL = "https://task-mangement-app-one.vercel.app/";
+export const  BASE_URL = "http://task-mangement-app-one.vercel.app/";
